31f1f6207815b9c53894733d86f81864f96cf17b
Goruchie <marcosgualtero43253@gmail.com>
2023-01-06 19:40:59 -0300

    decorate classes

--- person.rb
+++ person.rb
@@ -1,21 +1,29 @@
-class Person
+require_relative './nameable'
+
+class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age
+
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-
-  attr_reader :id
-  attr_accessor :name, :age
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
-  private
-
   def of_age?
     @age >= 18
+  end
+  private :of_age?
+  def can_use_services?
+    if of_age? || @parent_permission
+      true
+    else
+      false
+    end
+  end
+
+  def correct_name
+    @name
   end
 end
